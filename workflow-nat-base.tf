21f0c0ec8a57a8b9cb69f59acf442c3f24a7dddf
BTower Labz <labz@btower.net>
2020-08-01 11:51:41 +0000

    added nat-base workflows

--- workflow-nat-base.tf
+++ workflow-nat-base.tf
@@ -1,50 +1,43 @@
-/*
-module "workflow_vpc_pub2x_pri2x_dbs2x_no_nat" {
+module "workflow_nat_base_dev" {
   source            = "./modules/terraform-google-btlabz-tf-workflow"
   project           = data.google_project.project.project_id
   repo_owner        = "btower-labz"
-  repo_name         = "vpc-pub2x-pri2x-dbs2x-no-nat"
-  terraform_version = "0.11.14"
+  repo_name         = "terraform-aws-btlabz-nat-base"
+  repo_reference    = "master"
+  terraform_version = "latest"
   workflows = [
     "lint_pull_request",
     "lint_branch",
+    "conftest_pull_request",
+    "conftest_branch",
+    "terratest_pull_request",
+    "terratest_branch",
   ]
 }
-*/
 
-/*
-module "workflow_vpc_pub3x_pri3x_dbs3x_no_nat" {
+module "workflow_nat_base_12" {
   source            = "./modules/terraform-google-btlabz-tf-workflow"
   project           = data.google_project.project.project_id
   repo_owner        = "btower-labz"
-  repo_name         = "vpc-pub2x-pri2x-dbs2x-no-nat"
-  terraform_version = "0.11.14"
+  repo_name         = "terraform-aws-btlabz-nat-base"
+  repo_reference    = "release/terraform-12"
+  terraform_version = "0.12.29"
   workflows = [
     "lint_pull_request",
     "lint_branch",
-  ]
-}
-*/
-
-
-module "workflow_vpc_ha_2x" {
-  source            = "./modules/terraform-google-btlabz-tf-workflow"
-  project           = data.google_project.project.project_id
-  repo_owner        = "btower-labz"
-  repo_name         = "terraform-aws-btlabz-vpc-ha-2x"
-  terraform_version = "0.11.14"
-  workflows = [
-    "lint_pull_request",
-    "lint_branch",
+    "conftest_pull_request",
+    "conftest_branch",
+    "terratest_pull_request",
+    "terratest_branch",
   ]
 }
 
-
-module "workflow_vpc_ha_3x" {
+module "workflow_nat_base_11" {
   source            = "./modules/terraform-google-btlabz-tf-workflow"
   project           = data.google_project.project.project_id
   repo_owner        = "btower-labz"
-  repo_name         = "terraform-aws-btlabz-vpc-ha-3x"
+  repo_name         = "terraform-aws-btlabz-nat-base"
+  repo_reference    = "release/terraform-11"
   terraform_version = "0.11.14"
   workflows = [
     "lint_pull_request",
