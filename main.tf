ddf6c7b1756b6a4bdb69bee28dabb9d664541833
BTower Labz <labz@btower.net>
2020-08-01 15:23:44 +0000

    added vpc workflows

--- main.tf
+++ main.tf
@@ -25,29 +25,3 @@ module "workflow_vpc_pub3x_pri3x_dbs3x_no_nat" {
   ]
 }
 */
-
-
-module "workflow_vpc_ha_2x" {
-  source            = "./modules/terraform-google-btlabz-tf-workflow"
-  project           = data.google_project.project.project_id
-  repo_owner        = "btower-labz"
-  repo_name         = "terraform-aws-btlabz-vpc-ha-2x"
-  terraform_version = "0.11.14"
-  workflows = [
-    "lint_pull_request",
-    "lint_branch",
-  ]
-}
-
-
-module "workflow_vpc_ha_3x" {
-  source            = "./modules/terraform-google-btlabz-tf-workflow"
-  project           = data.google_project.project.project_id
-  repo_owner        = "btower-labz"
-  repo_name         = "terraform-aws-btlabz-vpc-ha-3x"
-  terraform_version = "0.11.14"
-  workflows = [
-    "lint_pull_request",
-    "lint_branch",
-  ]
-}
---
+++ workflow-vpc-ha-2x.tf
@@ -0,0 +1,46 @@
+module "workflow_vpc_ha_2x_dev" {
+  source            = "./modules/terraform-google-btlabz-tf-workflow"
+  project           = data.google_project.project.project_id
+  repo_owner        = "btower-labz"
+  repo_name         = "terraform-aws-btlabz-vpc-ha-2x"
+  repo_reference    = "master"
+  terraform_version = "latest"
+  workflows = [
+    "lint_pull_request",
+    "lint_branch",
+    "conftest_pull_request",
+    "conftest_branch",
+    "terratest_pull_request",
+    "terratest_branch",
+  ]
+}
+
+module "workflow_vpc_ha_2x_12" {
+  source            = "./modules/terraform-google-btlabz-tf-workflow"
+  project           = data.google_project.project.project_id
+  repo_owner        = "btower-labz"
+  repo_name         = "terraform-aws-btlabz-vpc-ha-2x"
+  repo_reference    = "release/terraform-12"
+  terraform_version = "0.12.29"
+  workflows = [
+    "lint_pull_request",
+    "lint_branch",
+    "conftest_pull_request",
+    "conftest_branch",
+    "terratest_pull_request",
+    "terratest_branch",
+  ]
+}
+
+module "workflow_vpc_ha_2x_11" {
+  source            = "./modules/terraform-google-btlabz-tf-workflow"
+  project           = data.google_project.project.project_id
+  repo_owner        = "btower-labz"
+  repo_name         = "terraform-aws-btlabz-vpc-ha-2x"
+  repo_reference    = "release/terraform-11"
+  terraform_version = "0.11.14"
+  workflows = [
+    "lint_pull_request",
+    "lint_branch",
+  ]
+}
