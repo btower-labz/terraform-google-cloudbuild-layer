8590fe730112b28ef675264a284383a36dbf3069
BTower Labz <labz@btower.net>
2021-10-09 15:49:57 +0000

    bumped to 1.0.8

--- modules/terraform-google-btlabz-conftest-pr/github-workflow-selector.tf
+++ modules/terraform-google-btlabz-conftest-pr/github-workflow-selector.tf
@@ -4,22 +4,22 @@ locals {
 
   // GitHUB Pull Request Workflow
   # TODO: #5 Review comment control options
-  pr_workflow = var.workflow_type == "PR" ? list(map(
-    "branch", var.repo_reference,
-    "invert_regex", var.invert_regex,
-    "comment_control", var.comment_control == "" ? null : var.comment_control,
-  )) : []
+  pr_workflow = var.workflow_type == "PR" ? tolist([tomap({
+    "branch" : var.repo_reference,
+    "invert_regex" : var.invert_regex,
+    "comment_control" : var.comment_control == "" ? null : var.comment_control,
+  })]) : []
 
   // GitHUB Branch Workflow
-  branch_workflow = var.workflow_type == "BRANCH" ? list(map(
-    "branch", var.repo_reference,
-    "invert_regex", var.invert_regex,
-  )) : []
+  branch_workflow = var.workflow_type == "BRANCH" ? tolist([tomap({
+    "branch" : var.repo_reference,
+    "invert_regex" : var.invert_regex,
+  })]) : []
 
   // GitHUB Tag Workflow
-  tag_workflow = var.workflow_type == "TAG" ? list(map(
-    "tag", var.repo_reference,
-    "invert_regex", var.invert_regex,
-  )) : []
+  tag_workflow = var.workflow_type == "TAG" ? tolist([tomap({
+    "tag" : var.repo_reference,
+    "invert_regex" : var.invert_regex,
+  })]) : []
 
 }
