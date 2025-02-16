3d4440a9fe21bc9149cb1d324e2932ff74658f58
Black Tower Labz <30019025+btower-labz@users.noreply.github.com>
2025-02-16 02:35:08 +0100

    Update workflow-docker-cloudbuild-terratest.tf

--- workflow-docker-cloudbuild-terratest.tf
+++ workflow-docker-cloudbuild-terratest.tf
@@ -12,6 +12,20 @@ module "workflow_docker_cloudbuild_terratest_dev" {
   ]
 }
 
+module "workflow_docker_cloudbuild_terratest_1_10_x" {
+  source            = "./modules/terraform-google-btlabz-docker-workflow"
+  project           = data.google_project.project.project_id
+  repo_owner        = "btower-labz"
+  repo_name         = "docker-cloudbuild-terratest"
+  repo_reference    = "release/1.10.x"
+  docker_registry   = "public.ecr.aws/v7k3l7g9"
+  docker_repository = "docker-cloudbuild-terratest"
+  docker_tag        = "tf-1.10.x-latest"
+  workflows = [
+    "docker_branch",
+  ]
+}
+
 module "workflow_docker_cloudbuild_terratest_1_2_x" {
   source            = "./modules/terraform-google-btlabz-docker-workflow"
   project           = data.google_project.project.project_id
